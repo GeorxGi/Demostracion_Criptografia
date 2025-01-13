d38bf795303a974b68754f586068fea45bccbac0
GeorxGi <georgechakour2004@gmail.com>
2025-01-13 19:07:02 -0400

    Agregar archivos de proyecto.

---
+++ Encriptador.cs
@@ -0,0 +1,90 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace PruebaCriptografia
+{
+    public static class Encriptador
+    {
+        public static string Encriptar(string plainText, string key)
+        {
+            try
+            {
+                // Instancia el objeto que posee el algoritmo de encriptación Aes
+                using Aes aes = Aes.Create();
+                //Asigna la llave de encriptación a partir de la llave ingresada
+                aes.Key = SHA256.HashData(Encoding.UTF8.GetBytes(key));
+                //Genera un vector de inicialización aleatorio
+                aes.GenerateIV();
+                //Asigna el modo de relleno
+                aes.Padding = PaddingMode.PKCS7;
+                //Crea un objeto que encripta el flujo de datos
+                using ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                //Crea un flujo de memoria
+                using MemoryStream ms = new MemoryStream();
+                //Escribe el vector de inicialización al flujo de memoria
+                ms.Write(aes.IV, 0, aes.IV.Length);
+                //Crea un flujo de encriptación
+                using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                //Crea un escritor que escribe en el flujo de encriptación
+                using (StreamWriter sw = new StreamWriter(cs))
+                {
+                    //Escribe el texto plano en el flujo de encriptación
+                    sw.Write(plainText);
+                }
+                //Convierte el flujo de memoria a un arreglo de bytes y lo convierte a una cadena en base 64
+                return Convert.ToBase64String(ms.ToArray());
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+                return "";
+            }
+        }
+
+        public static string Desencriptar(string cipherText, string key)
+        {
+            try
+            {
+                //Convierte el string de base 64 a un arreglo de bytes
+                byte[] fullCipher = Convert.FromBase64String(cipherText);
+                //Crea un arreglo de bytes para el vector de inicialización
+                byte[] iv = new byte[16];
+                //Copia los primeros 16 bytes del arreglo de bytes encriptado al vector de inicialización
+                Array.Copy(fullCipher, 0, iv, 0, iv.Length);
+                //Crea un arreglo de bytes para el texto encriptado
+                byte[] cipher = new byte[fullCipher.Length - iv.Length];
+                //Copia los bytes restantes del arreglo de bytes encriptado al arreglo de bytes del texto encriptado
+                Array.Copy(fullCipher, iv.Length, cipher, 0, cipher.Length);
+
+                //Instancia el objeto que posee el algoritmo de encriptación Aes
+                using Aes aes = Aes.Create();
+                //Asigna la llave de encriptación a partir de la llave ingresada
+                aes.Key = SHA256.HashData(Encoding.UTF8.GetBytes(key));
+                //Asigna el vector de inicialización
+                aes.IV = iv;
+                //Asigna el modo de relleno
+                aes.Padding = PaddingMode.PKCS7;
+
+                //Crea un objeto que desencripta el flujo de datos
+                using ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                //Crea un flujo de memoria
+                using MemoryStream ms = new MemoryStream(cipher);
+                //Crea un flujo de desencriptación
+                using CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+                //Crea un lector que lee del flujo de desencriptación
+                using StreamReader sr = new StreamReader(cs);
+                //Devuelve el texto desencriptado
+                return sr.ReadToEnd();
+            }
+            catch(Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(e.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+                return "";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace PruebaCriptografia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "";
+            char opt;
+            do
+            {
+                Mostrar(texto);
+                opt = Console.ReadKey().KeyChar;
+                switch (opt)
+                {
+
+                    case '1':
+                        Console.Clear();
+                        texto = Encriptador.Encriptar(texto, IngresarLlave());
+                        break;
+                    case '2':
+                        Console.Clear();
+                        string aux = Encriptador.Desencriptar(texto, IngresarLlave());
+                        Console.WriteLine($"Tu mensaje no encriptado es: \"{aux}\"?\nS-Si\nN-No\n");
+                        char opt2 = Console.ReadKey().KeyChar;
+                        texto = (opt2 == 'S' || opt2 == 's') ? aux : texto;
+                        break;
+                    case '3':
+                        Console.Clear();
+                        Console.WriteLine("Ingrese el mensaje:");
+                        texto = Console.ReadLine();
+                        break;
+                    default:
+                        break;
+                }
+                Console.Clear();
+            } while (opt != '0');
+        }
+
+        private static void Mostrar(string texto)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Texto actual = {texto}\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("1-Encriptar mensaje\n2-Desencriptar mensaje\n3-Ingresar nuevo mensaje\n0-Salir");
+        }
+        private static string IngresarLlave()
+        {
+            string key;
+            //Recibe la llave de encriptación
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Ingrese la clave de encriptación");
+                Console.ForegroundColor = ConsoleColor.White;
+                key = Console.ReadLine();
+            } while (string.IsNullOrWhiteSpace(key));
+
+            return key;
+        }
+    }
+}
